9f95d818ce9a819804075c6219e5a41ff0378784
cybercoder-naj <cybercoder.nishant@gmail.com>
2021-08-07 17:49:06 +0530

    Merge branch 'world-clock' into master

--- app/src/main/java/com/nishant/compose/MainActivity.kt
+++ app/src/main/java/com/nishant/compose/MainActivity.kt
@@ -1,10 +1,8 @@
-package com.nishant.compose.ui.activities
+package com.nishant.compose
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import com.nishant.compose.ui.activities.viewmodels.MainViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
