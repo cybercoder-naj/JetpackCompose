176b8fdf86b3f1ec64c10e83ba4c003ea1d68859
cybercoder-naj <cybercoder.nishant@gmail.com>
2021-08-07 14:00:36 +0530

    fix

--- app/src/main/java/com/nishant/compose/MainActivity.kt
+++ app/src/main/java/com/nishant/compose/MainActivity.kt
@@ -6,30 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.PointMode
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.layout.onSizeChanged
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import kotlinx.coroutines.delay
-import kotlin.math.PI
-import kotlin.math.cos
-import kotlin.math.sin
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,6 +33,26 @@ class MainActivity : ComponentActivity() {
                     )
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun ProgressBar(current: Float, total: Float) {
+    val progress = current / total
+
+    Box(
+        modifier = Modifier
+            .width(200.dp)
+            .height(200.dp)
+    ) {
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            drawArc(
+                Color.Green,
+                -90f,
+                360f * progress,
+                false
+            )
         }
     }
 }
@@ -96,7 +98,7 @@ fun Timer(
     ) {
         Canvas(modifier = modifier) {
             drawArc(
-                color = inactiveBarColor,
+                color = inactiveBarColor,`
                 startAngle = -215f,
                 sweepAngle = 250f,
                 useCenter = false,
